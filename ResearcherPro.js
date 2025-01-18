f39d2d74ab5bf1620e5844b0943c40ec838e8f7a
mahiratuba8 <mahiratuba8@gmail.com>
2025-01-18 23:45:03 +0100

    changes

--- ResearcherPro.js
+++ ResearcherPro.js
@@ -1,3 +1,13 @@
+document.querySelectorAll('.faq-question').forEach(question => {
+    question.addEventListener('click', () => {
+        const answer = question.nextElementSibling;
 
+        if (answer.style.display === 'block') {
+            answer.style.display = 'none';
+        } else {
+            answer.style.display = 'block';
+        }
+    });
+});
 
 
